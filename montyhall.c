e218e79c5541ccae780f7e3aa55d7d7e9c54a974
BrunoZ31 <51855024+BrunoZ31@users.noreply.github.com>
2019-06-18 23:42:22 -0300

    final version
    In this one I used more random numbers, for example using them to see if the person would change the door or keep it.

--- montyhall.c
+++ montyhall.c
@@ -16,76 +16,53 @@ int RandomInteger(int low, int high)
 
 int main()
 {   
-	//SETAR UM NUMERO (PORTA) QUE É O NUMERO VENCEDOR
-	//FAZER O TIPO QUE TROCA O NUMERO (A PORTA) E O QUE NÃO TROCA
-	//COMPARAR OS RESULTADOS  
+	//FAZER UM VETOR QUE ARMAZENE: VEZ JOGADA; PORTA ESCOLHIDA; PORTA GANHADORA; TROCA OU NÃO TROCA
 	//
-	long long int n=100000;
-	long long int ESCOLHA[n];
-	long double somaTROCA=0,somaNAOTROCA=0;
+	// 
+	//
+	long long int n=100000, ESCOLHAS[n][3];
+	long double TROCA=0,NAO_TROCA=0;
 
-//TROCA DE PORTA
-	for (int i = 0; i < n; ++i)
+	for (int vezjogada= 0; vezjogada < n; ++vezjogada)
 	{
-//		printf("%d ",i); 
-
-		ESCOLHA[i]=RandomInteger(1,3);
+	//ESCOLHAS SIMPLES 
 		
-//		printf("ESCOLHA: %ld ",ESCOLHA[i]);
+		ESCOLHAS[vezjogada][0]=RandomInteger(1,3);//PORTA ESCOLHIDA
+		ESCOLHAS[vezjogada][1]=RandomInteger(1,3);//PORTA GANHADORA
+		ESCOLHAS[vezjogada][2]=RandomInteger(1,2);//TROCA(1) OU NÃO TROCA(2)
 
-		if(ESCOLHA[i]==1)
-		{
-		}
-		if(ESCOLHA[i]==2)
-		{
+		if(ESCOLHAS[vezjogada][0]==ESCOLHAS[vezjogada][1] && ESCOLHAS[vezjogada][2]==2)
+			{
+			NAO_TROCA++; //o unico caso que ganha quando nao troca é quando a porta escolhida e a ganhadora sao iguais
+		//	printf("+");
+			}
 
-					somaTROCA++;
-					//printf(" +");
-		}
 
-			/*SE ESCOLHER 1 O JOGADOR GANHOU*/
-		
-		if(ESCOLHA[i]==3)
-		{
-		
-			somaTROCA++;
-	//		printf(" +");
-		}
-//		printf("\n");
+		//é indiferente qual sera aberta, sendo que ela é sempre uma sem o prêmio
+		//portanto, basta eu estar em uma porta diferente da vencedora
+		//e com intenção de trocar que eu ganho
+
+		if (ESCOLHAS[vezjogada][0]!=ESCOLHAS[vezjogada][1] && ESCOLHAS[vezjogada][2]==1)
+			{
+				TROCA++;
+			}
+
+		//printf("PORTA ESCOLHIDA: %lld \tPORTA GANHADORA: %lld\tT ou NT: %lld\n",ESCOLHAS[vezjogada][0],ESCOLHAS[vezjogada][1],ESCOLHAS[vezjogada][2]);
+
 	}
 
-		printf("\n\n");
-//NÃO TROCA DE PORTA
-    
-	for (int i = 0; i < n; ++i)
-	{
-
-
-		ESCOLHA[i]=RandomInteger(1,3);
-		
-		//printf("\nESCOLHA: %lld ",ESCOLHA[i]);
-
-		if(ESCOLHA[i]==1)
-		{
-			somaNAOTROCA++;
-		
-		}
-		if(ESCOLHA[i]==2)
-		{
-		}
-		if(ESCOLHA[i]==3)
-		{
-		}
-
-	}
-	printf("\n\n");
-	printf("Numero de vezes ganhado quando TROCA de porta: %Lf \n", somaTROCA);
-	printf("Numero de vezes ganhado quando NAO TROCA de porta: %Lf \n",somaNAOTROCA);
+	printf("\n");
+	printf("Numero de vezes ganhado quando TROCA de porta: %Lf \n", TROCA);
+	printf("Numero de vezes ganhado quando NAO TROCA de porta: %Lf \n",NAO_TROCA);
 	printf("\n");
 
 	double fracao= 0;
-	fracao= (double)somaTROCA/(double)somaNAOTROCA;
-	printf("fração entre eles: %lf\n", fracao);
 
+	fracao= (double)TROCA/(double)NAO_TROCA;
+
+	printf("\t\tfração entre eles: %lf\n", fracao);
+	printf("fração de TROCA/n= %Lf \t NAO_TROCA/n= %Lf \n", TROCA/(double)n, NAO_TROCA/(double)n);
+    
+	//podemos observar que a fração tende a 2. indicando que o tu tem o dobro de chance se trocar de porta
     return 0;
 }
